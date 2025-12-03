b26fcc7f4160186f88f20ba566cdc7e1cb544373
harshakhandavilli2001 <112113282+harshakhandavilli2001@users.noreply.github.com>
2025-12-03 18:52:28 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-  account: "198884080916",        // Friend’s AWS Account ID
+  account: "xxxx",        // Friend’s AWS Account ID
   region: "ap-south-2",            // AWS Region
-  accessKeyId: "AKIAS4TTKHUKNZ6WB5XV",
-  secretAccessKey: "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",
+  accessKeyId: "xxxx",
+  secretAccessKey: "xxxx",
 };
